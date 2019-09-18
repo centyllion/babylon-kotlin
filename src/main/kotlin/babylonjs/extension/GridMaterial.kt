6c205b7ddc8b016784a6422cd29b9a95eceee3c9
Jean-Charles <jeancharles.roger@me.com>
2019-09-18 12:40:56 +0200

    Adds GridMaterial

---
+++ src/main/kotlin/babylonjs/extension/GridMaterial.kt
@@ -0,0 +1,58 @@
+@file:JsQualifier("BABYLON")
+@file:Suppress("unused", "ConvertSecondaryConstructorToPrimary", "CovariantEquals", "FunctionName", "PropertyName")
+package babylonjs.extension
+
+import babylonjs.AbstractMesh
+import babylonjs.Color3
+import babylonjs.PushMaterial
+import babylonjs.Scene
+import babylonjs.SubMesh
+import babylonjs.Vector3
+
+external class GridMaterial: PushMaterial {
+    /**
+     * Main color of the grid (e.g. between lines)
+     */
+    var mainColor: Color3
+    /**
+     * Color of the grid lines.
+     */
+    var lineColor: Color3
+    /**
+     * The scale of the grid compared to unit.
+     */
+    var gridRatio: Number
+    /**
+     * Allows setting an offset for the grid lines.
+     */
+    var gridOffset: Vector3
+    /**
+     * The frequency of thicker lines.
+     */
+    var majorUnitFrequency: Number
+    /**
+     * The visibility of minor units in the grid.
+     */
+    var minorUnitVisibility: Number
+    /**
+     * The grid opacity outside of the lines.
+     */
+    var opacity: Number
+    /**
+     * Determine RBG output is premultiplied by alpha value.
+     */
+    var preMultiplyAlpha: Boolean
+    /**
+     * constructor
+     * @param name The name given to the material in order to identify it afterwards.
+     * @param scene The scene the material is used in.
+     */
+    constructor(name: String, scene: Scene)
+    
+    fun isReadyForSubMesh(mesh: AbstractMesh, subMesh: SubMesh, useInstances: Boolean?): Boolean
+    fun dispose(forceDisposeEffect: Boolean?)
+
+    companion object {
+        fun Parse(source: Any, scene: Scene, rootUrl: String): GridMaterial
+    }
+}
